f8fefedc94206363425f9c9074138b7785c2e27b
yjchen225 <yjchen225@gmail.com>
2025-12-13 22:32:01 +0800

    Change project name and path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/monthly-billing-app/',
+  base: '/Ragic-reports-monthly-billing/',
 })
